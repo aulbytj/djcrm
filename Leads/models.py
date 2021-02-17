df374b99098c334888a1c8e82de3df56cdb4ed48
Aulbourn <aulbourn@gmail.com>
2021-02-17 11:59:28 -0500

    add foreign key to lead model

--- Leads/models.py
+++ Leads/models.py
@@ -4,21 +4,13 @@ from django.db import models
 
 class Lead(models.Model):
 
-  SOURCE_CHOICES = (
-    # the first value will be stored to the database and the second value is what will be displayed
-    ('Youtube', 'Youtube'),
-    ('Google', 'Google'),
-    ('Newsletter', 'Newletter'),
-  )
-
   first_name = models.CharField(max_length=20)
   last_name = models.CharField(max_length=20)
   age = models.IntegerField(default=0)
+  # this states that a lead can have one Agent but an Agent can have many leads
+  agent = models.ForeignKey('Agent', on_delete=models.CASCADE)
 
-  phoned = models.BooleanField()
-  source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
 
-  #  blank=True means that it can be an empty string
-  #  null=True means that it can be NULL will be stored in the database
-  profile_picture = models.ImageField(blank=True, null=True)
-  special_files = models.FileField(blank=True, null=True)
+class Agent(models.Model):
+  first_name = models.CharField(max_length=20)
+  last_name = models.CharField(max_length=20)
